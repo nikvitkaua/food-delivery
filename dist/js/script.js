22e4b617edc22e8f5225d134313d7309588d7c16
Mykyta Paziuka <m.paziuka@tsquad.net>
2025-07-18 13:36:46 +0300

    add script fro tabs on site

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,27 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tabContainer = document.querySelector(".tabcontainer"),
+    tabs = tabContainer.querySelectorAll(".tabcontent"),
+    tabsTitle = tabContainer.querySelectorAll(".tabheader__item");
+  tabsTitle.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      removeActiveClass(tabContainer, "tabheader__item_active");
+      item.classList.add("tabheader__item_active");
+      removeActiveClass(tabContainer, "tabcontent--active");
+      tabs[i].classList.add("tabcontent--active");
+    });
+  });
+  function removeActiveClass(parent, activeClass) {
+    parent.querySelector(`.${activeClass}`).classList.remove(activeClass);
+  }
+  ;
+});
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabContainer = document.querySelector(".tabcontainer"),
+        tabs = tabContainer.querySelectorAll(".tabcontent"),
+        tabsTitle = tabContainer.querySelectorAll(".tabheader__item");
+
+    tabsTitle.forEach((item, i) => {
+        item.addEventListener("click", () => {
+            removeActiveClass(tabContainer, "tabheader__item_active");
+            item.classList.add("tabheader__item_active");
+
+            removeActiveClass(tabContainer, "tabcontent--active");
+            tabs[i].classList.add("tabcontent--active");
+        });
+    });
+
+    function removeActiveClass(parent, activeClass) {
+        parent.querySelector(`.${activeClass}`).classList.remove(activeClass);
+    };
+});
