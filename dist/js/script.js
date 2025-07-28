e6f601b26389573f618c57bbd24a6524a780ab65
Mykyta Paziuka <m.paziuka@tsquad.net>
2025-07-28 20:22:10 +0300

    add functional to timer if date  <= 0

--- dist/js/script.js
+++ dist/js/script.js
@@ -30,11 +30,19 @@ document.addEventListener("DOMContentLoaded", () => {
     month: "long"
   }).format(new Date(deadline))}`;
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor(t / (1000 * 60 * 60) % 24),
-      minutes = Math.floor(t / 1000 / 60 % 60),
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor(t / (1000 * 60 * 60) % 24);
+      minutes = Math.floor(t / 1000 / 60 % 60);
       seconds = Math.floor(t / 1000 % 60);
+    }
     return {
       total: t,
       days: days,
