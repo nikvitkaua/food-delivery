b4d33321f1010c290b42ebc6f82c2af9766ece5d
Mykyta Paziuka <m.paziuka@tsquad.net>
2025-08-10 19:52:58 +0300

    add: slider

--- js/script.js
+++ js/script.js
@@ -282,4 +282,46 @@ window.addEventListener("DOMContentLoaded", function () {
     fetch("http://localhost:3000/menu ")
         .then((data) => data.json())
         .then((res) => console.log(res));
+
+    // Slider
+
+    const slides = document.querySelectorAll(".offer__slide"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current");
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+    
+    total.textContent = getZero(slides.length);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = "none");
+
+        slides[slideIndex - 1].style.display = "block";
+
+        current.textContent = getZero(slideIndex);
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener("click", () => {
+        plusSlides(1);
+    });
 });
